09cb3a7f00edb19cce4142c68feb7c4f3a6f4d7a
jaymo99 <karisjaymo99@gmail.com>
2022-06-02 07:57:49 +0300

    add Navbar

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>easyfix</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="navbar-container">
+        <div class="navbar">
+            
+            <div class="logo">
+                <span>EASYFIX</span>
+            </div>
+            <ul>
+                <li> <a>LOG IN</a> </li>
+                <li>SIGN UP</li>
+            </ul>
+        </div>
+    </div>
+    <div class="body_container">
+        <div class="map">
+
+        </div>
+    </div>
+</body>
+</html>
