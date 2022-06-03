c2407e7f097b2148648ef4f067ad3a3d319621c5
jaymo99 <karisjaymo99@gmail.com>
2022-06-03 15:54:05 +0300

    complete index page

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
                 <span>EASYFIX</span>
             </div>
             <ul>
-                <li> <a>LOG IN</a> </li>
-                <li>SIGN UP</li>
+                <li><a href="#">LOG IN</a></li>
+                <li><a href="#">SIGN UP</a></li>
             </ul>
         </div>
     </div>
@@ -24,6 +24,34 @@
         <div class="map">
 
         </div>
+
+        <div class="index_page_cards">
+            <span>Mechanics Near You</span>
+            <div class="mechanic_cards_container">
+                <div class="mechanic_card">
+                    <img src="graphics/mechanic.jpg" alt="">
+                    <p>Mr. Handyman and Sons</p>
+                    <span>Rongai</span>
+                </div>
+                <div class="mechanic_card">
+                    <img src="graphics/mechanic.jpg" alt="">
+                    <p>Mr. Handyman and Sons</p>
+                    <span>Rongai</span>
+                </div>
+                <div class="mechanic_card">
+                    <img src="graphics/mechanic.jpg" alt="">
+                    <p>Mr. Handyman and Sons</p>
+                    <span>Rongai</span>
+                </div>
+                <div class="mechanic_card">
+                    <img src="graphics/mechanic.jpg" alt="">
+                    <p>Mr. Handyman and Sons</p>
+                    <span>Rongai</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
     </div>
 </body>
 </html>
