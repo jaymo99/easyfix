402de532dc36b417c7e6c5da3a719405adce8e33
jaymo99 <karisjaymo99@gmail.com>
2022-06-04 09:33:40 +0300

    add login page UI

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="my-navbar-container">
+        <div class="my-navbar">
+            
+            <div class="my-logo">
+                <span>EASYFIX</span>
+            </div>
+            <ul>
+                <li><a href="#">LOG IN</a></li>
+                <li><a href="#">SIGN UP</a></li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="body_container center">
+        <div class="mech-register">
+            <p>LOG IN</p>
+            <form action="">
+                <select name="" id="" class="form-control" required>
+                    <option value="" selected disabled>Choose an Option</option>
+                    <option value="">I'm a Mechanic</option>
+                    <option value="">I'm a Client</option>
+                    </select>
+                <input type="email" name="" id="" class="form-control" placeholder="Email" required>
+                <input type="password" name="" id="" class="form-control" placeholder="Password" required>
+                <input type="submit" value="SUBMIT" class="btn btn-dark btn-width">
+            </form>
+        </div>
+    </div>
+
+    <script src="bootstrap/js/bootstrap.bundle.js"></script>
+</body>
+</html>
