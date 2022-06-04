50772ff3f936766d490ec1296dc990271cabbb69
jaymo99 <karisjaymo99@gmail.com>
2022-06-04 19:39:53 +0300

    added recent appointments card collection

---
+++ customer-appointments.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My appointments</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="my-navbar-container">
+        <div class="my-navbar">
+            
+            <div class="my-logo">
+                <span>EASYFIX</span>
+            </div>
+            <ul>
+                <li><a href="#">LOG IN</a></li>
+                <li><a href="#">SIGN UP</a></li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="body_container">
+        <div class="recent-appointments">
+            <h1>RECENT APPOINTMENTS</h1>
+            <div class="appointments-collection">
+                <div class="appointment-card-container">
+                    <div class="appointment-card">
+                        <div class="appointment-card-title">
+                            <span>Urban Motors General Servicing and Repair Center</span>
+                        </div>
+                        <div class="appointment-card-content">
+                            <div class="appointment-card-col">
+                                <img src="graphics/stopwatch.png" alt="">
+                                <div class="appointment-card-col2">
+                                    <span class="span-status">Pending Approval</span>
+                                    <span class="span-date">15-Feb-2022</span>
+                                </div>
+                            </div>
+                            <button class="btn btn-dark btn-sm">DETAILS</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="appointment-card-container">
+                    <div class="appointment-card">
+                        <div class="appointment-card-title">
+                            <span>Urban Motors General Servicing and Repair Center</span>
+                        </div>
+                        <div class="appointment-card-content">
+                            <div class="appointment-card-col">
+                                <img src="graphics/stopwatch.png" alt="">
+                                <div class="appointment-card-col2">
+                                    <span class="span-status">Pending Approval</span>
+                                    <span class="span-date">15-Feb-2022</span>
+                                </div>
+                            </div>
+                            <button class="btn btn-dark btn-sm">DETAILS</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <script src="bootstrap/js/bootstrap.bundle.js"></script>
+</body>
+</html>
