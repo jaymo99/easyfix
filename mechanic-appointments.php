b24ebf2432941af5244065a523769b3db2b77e33
jaymo99 <karisjaymo99@gmail.com>
2022-06-05 15:24:52 +0300

    complete mechanic-appointments UI

---
+++ mechanic-appointments.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My appointments</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="my-navbar-container">
+        <div class="my-navbar">
+            
+            <div class="my-logo">
+                <span>EASYFIX</span>
+            </div>
+            <ul>
+                <li><a href="#">LOG IN</a></li>
+                <li><a href="#">SIGN UP</a></li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="body_container">
+        <div class="recent-appointments">
+            <h1 class="section-heading">UPCOMING APPOINTMENTS</h1>
+            <div class="appointments-collection">
+                <div class="appointment-card-container service-upcoming">
+                    <div class="appointment-card">
+                        <div class="mech-appointment-title">
+                            <span class="appointment-title-one">James Mwaura Kariuki</span>
+                            <span class="appointment-title-two">Range rover Sport</span>
+                        </div>
+                        <div class="appointment-card-content">
+                            <div class="appointment-card-col">
+                                <img src="graphics/appointment.png" alt="">
+                                <div class="appointment-card-col2">
+                                    <span class="span-status">15-Feb-2022</span>
+                                    <span class="span-date">08:00 HRS</span>
+                                </div>
+                            </div>
+                            <button class="btn btn-dark btn-sm">DETAILS</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="appointment-card-container service-upcoming">
+                    <div class="appointment-card">
+                        <div class="mech-appointment-title">
+                            <span class="appointment-title-one">James Mwaura Kariuki</span>
+                            <span class="appointment-title-two">Range rover Sport</span>
+                        </div>
+                        <div class="appointment-card-content">
+                            <div class="appointment-card-col">
+                                <img src="graphics/appointment.png" alt="">
+                                <div class="appointment-card-col2">
+                                    <span class="span-status">15-Feb-2022</span>
+                                    <span class="span-date">08:00 HRS</span>
+                                </div>
+                            </div>
+                            <button class="btn btn-dark btn-sm">DETAILS</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="recent-appointments">
+            <h1 class="section-heading">PENDING APPROVAL</h1>
+            <div class="appointments-collection">
+                <div class="appointment-card-container service-pending">
+                    <div class="appointment-card">
+                        <div class="mech-appointment-title">
+                            <span class="appointment-title-one">Andrew Robertson</span>
+                            <span class="appointment-title-two">Honda Civic</span>
+                        </div>
+                        <div class="appointment-card-content">
+                            <div class="appointment-card-col">
+                                <img src="graphics/stopwatch.png" alt="">
+                                <div class="appointment-card-col2">
+                                    <span class="span-status">15-Feb-2022</span>
+                                    <span class="span-date">08:00 HRS</span>
+                                </div>
+                            </div>
+                            <button class="btn btn-dark btn-sm">DETAILS</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+    <script src="bootstrap/js/bootstrap.bundle.js"></script>
+</body>
+</html>
