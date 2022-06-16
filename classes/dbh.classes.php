55a9bbf25d779a0709cb7da33634ce664012f074
jaymo99 <karisjaymo99@gmail.com>
2022-06-16 15:29:49 +0300

    add remote database connection

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -4,9 +4,16 @@ class Dbh{
 
     protected function connect(){
       try {
-        $username = "root";
-        $password = "";
-        $dbh = new PDO('mysql:host=localhost;dbname=easyfix', $username, $password);
+        //localhost database connection
+        // $username = "root";
+        // $password = "";
+        // $dbh = new PDO('mysql:host=localhost;dbname=easyfix', $username, $password);
+        // return $dbh;
+
+        //remote database connection
+        $username = "ukUyeOj4W9";
+        $password = "9cR4Ci60aQ";
+        $dbh = new PDO('mysql:host=remotemysql.com;dbname=ukUyeOj4W9', $username, $password);
         return $dbh;
       }
       catch (PDOException $e) {
