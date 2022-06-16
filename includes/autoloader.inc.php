da97cfc9ed9484dbf0e5017cba05330cb65920a4
jaymo99 <karisjaymo99@gmail.com>
2022-06-16 20:45:53 +0300

    added __dir__ to my original includes

--- includes/autoloader.inc.php
+++ includes/autoloader.inc.php
@@ -5,17 +5,17 @@ define('BASEURL', $_SERVER['DOCUMENT_ROOT']);
 spl_autoload_register('myAutoloader');
 
 function myAutoloader($className) {
-    if(file_exists("classes/" . $className . ".classes.php")){
-        include "classes/" . $className . ".classes.php";
+    if(file_exists(__DIR__ . "classes/" . $className . ".classes.php")){
+        require_once __DIR__ . "classes/" . $className . ".classes.php";
     }
 
-    if(file_exists("../classes/" .$className. ".classes.php")){
-        include "../classes/" .$className. ".classes.php";
+    if(file_exists(__DIR__ . "../classes/" .$className. ".classes.php")){
+        require_once __DIR__ . "../classes/" .$className. ".classes.php";
     }
 
-    $path = BASEURL . '/classes/';
-    $extension = '.classes.php';
-    $fullPath = $path . $className . $extension;
+    // $path = '/classes/';
+    // $extension = '.classes.php';
+    // $fullPath = $path . $className . $extension;
 
-    require_once __DIR__ . $path . strtr($className, "\\", "/") . $extension;
+    // require_once __DIR__ . $path . strtr($className, "\\", "/") . $extension;
 }
