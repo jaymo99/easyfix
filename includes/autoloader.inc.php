f9c3f5e47498f3c1f5acf36e099e7f5c231e2adf
jaymo99 <karisjaymo99@gmail.com>
2022-06-16 21:11:52 +0300

    capitalize class names

--- includes/autoloader.inc.php
+++ includes/autoloader.inc.php
@@ -5,17 +5,12 @@ define('BASEURL', $_SERVER['DOCUMENT_ROOT']);
 spl_autoload_register('myAutoloader');
 
 function myAutoloader($className) {
-    if(file_exists(__DIR__ . "classes/" . strtr($className, "\\", "/") . ".classes.php")){
-        require_once __DIR__ . "classes/" . strtr($className, "\\", "/") . ".classes.php";
+    if(file_exists("classes/" . $className . ".classes.php")){
+        include "classes/" . $className . ".classes.php";
     }
 
-    if(file_exists(__DIR__ . "../classes/" .strtr($className, "\\", "/"). ".classes.php")){
-        require_once __DIR__ . "../classes/" .strtr($className, "\\", "/"). ".classes.php";
+    if(file_exists("../classes/" .$className. ".classes.php")){
+        include "../classes/" .$className. ".classes.php";
     }
 
-    // $path = '/classes/';
-    // $extension = '.classes.php';
-    // $fullPath = $path . $className . $extension;
-
-    // require_once __DIR__ . $path . strtr($className, "\\", "/") . $extension;
 }
