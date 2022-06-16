ab33fb7edd10fddc10de2d6dd6e4533dcc6d3a7a
jaymo99 <karisjaymo99@gmail.com>
2022-06-16 18:36:27 +0300

    add baseurl

--- includes/autoloader.inc.php
+++ includes/autoloader.inc.php
@@ -1,5 +1,7 @@
 <?php 
 
+define('BASEURL', $_SERVER['DOCUMENT_ROOT']);
+
 spl_autoload_register('myAutoloader');
 
 function myAutoloader($className) {
@@ -10,4 +12,10 @@ function myAutoloader($className) {
     if(file_exists("../classes/" .$className. ".classes.php")){
         include "../classes/" .$className. ".classes.php";
     }
+
+    $path = BASEURL . '/classes';
+    $extension = '.classes.php';
+    $fullPath = $path . $className . $extension;
+
+    require $fullPath;
 }
