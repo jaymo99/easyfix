75aa07bbc295269af2dd71b8d66583fe67fcc91e
jaymo99 <karisjaymo99@gmail.com>
2022-06-26 19:22:34 +0300

    added settings.php file & settingsMap() function in map.js

--- JS/map.js
+++ JS/map.js
@@ -20,3 +20,37 @@ function initMap() {
 }
 
 window.initMap = initMap;
+
+//---------------------- MAP on mechanic-settings page -------------------
+function settingsMap() {
+  // The location of Maasai mall rongai
+  const rongai = { lat: -1.3942330517263632, lng: 36.764185816517866 };
+  // The map, centered at maasai mall rongai
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: rongai,
+  });
+
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to set your location!",
+    position: rongai,
+  });
+
+  infoWindow.open(map);
+
+  map.addListener("click", (mapsMouseEvent) => {
+    // Close the current InfoWindow.
+    infoWindow.close();
+
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
+    });
+    infoWindow.setContent(
+      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+    );
+    infoWindow.open(map);
+  });
+
+}
+
---
+++ mechanic-settings.php
@@ -0,0 +1,85 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My appointments</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <!--  -->
+    <script src="JS/script.js" defer></script>
+    <script src="JS/hamburger.js" defer></script>
+    <script src="JS/map.js" defer></script>
+    <script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDteNsv96-8loTY5QU0sjFFjQJGVnJQXPo&callback=settingsMap">
+    </script>
+    
+</head>
+<body>
+    <div class="my-navbar-container">
+        <div class="my-navbar">
+            
+            <div class="my-logo">
+                <a href="index.php"><span>EASYFIX</span></a>
+            </div>
+            <?php if(isset($_SESSION['user_id'])) { ?>
+                <div class="hamburger">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </div>
+
+                <ul class="my-menu">
+                    <div class="user-loggedIn">
+                        <li class="nav-list-item"> <a href="index.php">Home</a> </li>
+                        <li class="nav-list-item">
+                            <?php if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == "client") { ?>
+                                <a href="client-appointments.php">Appointments</a>
+                            <?php } elseif(isset($_SESSION['user_role']) && $_SESSION['user_role'] == "mechanic") { ?>
+                                <a href="mechanic-appointments.php">Appointments</a>
+                            <?php } ?>
+                            </li>
+                        <li class="nav-list-item"> <a href="#" class="active-link">Settings</a> </li>
+                        <li class="logout-btn">
+                            <a href="includes/logout.inc.php" class="btn btn-sm btn-secondary mybtn-nav btn-hamburger">LOG OUT</a>
+                            <!-- <img src="graphics/user.png" class="my-user-profile" alt="user-profile"> -->
+                        </li>
+                    </div>
+                <?php }else{ ?>
+
+                <div class="user-loggedOut">
+                    <div>
+                        <a href="login.php" class="btn btn-sm btn-secondary mybtn-nav">LOG IN</a>
+                    </div>
+                    <div class="dropdown" data-dropdown>
+                        <button class="btn btn-sm btn-secondary mybtn-nav" data-dropdown-button>SIGN UP</button>
+                        <div class="dropdown-menu">
+                            <a class="btn" href="client-signup.php">Client</a>
+                            <a class="btn" href="mechanic-signup.php">Mechanic</a>
+                        </div>
+                    </div>
+                </div>
+                <?php  } ?>
+
+            </ul>
+        </div>
+    </div>
+
+    <div class="body_container">
+        <div class="reduced_body">
+            <div id="map"></div>
+        </div>
+    </div>
+
+    <!-- <script src="bootstrap/js/bootstrap.bundle.js"></script> -->
+</body>
+</html>
